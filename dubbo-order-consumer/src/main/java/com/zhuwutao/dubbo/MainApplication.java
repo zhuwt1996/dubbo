2f0e2426ee1945a5c28d98d55da2b470ce20e678
zwt <1255284280@qq.com>
2019-06-13 17:40:38 +0800

    启动时检查

--- dubbo-order-consumer/src/main/java/com/zhuwutao/dubbo/MainApplication.java
+++ dubbo-order-consumer/src/main/java/com/zhuwutao/dubbo/MainApplication.java
@@ -13,8 +13,8 @@ public class MainApplication {
 
     public static void main(String[] args) throws IOException {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("consumer.xml");
-        OrderService orderService = (OrderService) context.getBean(OrderService.class);
-        orderService.initOrder("1");
+        //OrderService orderService = (OrderService) context.getBean(OrderService.class);
+        //orderService.initOrder("1");
         System.out.println("调用完成...");
         System.in.read();
     }
