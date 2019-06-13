0f959bc5538723064d791e47306320c1d0905335
zwt <1255284280@qq.com>
2019-06-13 17:54:29 +0800

    超时

--- dubbo-order-consumer/src/main/java/com/zhuwutao/dubbo/MainApplication.java
+++ dubbo-order-consumer/src/main/java/com/zhuwutao/dubbo/MainApplication.java
@@ -1,7 +1,6 @@
 package com.zhuwutao.dubbo;
 
 import com.zhuwutao.dubbo.service.OrderService;
-import com.zhuwutao.dubbo.service.UserService;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.io.IOException;
@@ -13,8 +12,8 @@ public class MainApplication {
 
     public static void main(String[] args) throws IOException {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("consumer.xml");
-        //OrderService orderService = (OrderService) context.getBean(OrderService.class);
-        //orderService.initOrder("1");
+        OrderService orderService = (OrderService) context.getBean(OrderService.class);
+        orderService.initOrder("1");
         System.out.println("调用完成...");
         System.in.read();
     }
--- dubbo-user-provider/src/main/java/com/zhuwutao/dubbo/service/impl/UserServiceImpl.java
+++ dubbo-user-provider/src/main/java/com/zhuwutao/dubbo/service/impl/UserServiceImpl.java
@@ -14,11 +14,11 @@ public class UserServiceImpl implements UserService {
         System.out.println("UserServiceImpl.....old...");
         UserAddress address1 = new UserAddress(1, "北京市昌平区宏福科技园综合楼3层", "1", "李老师", "010-56253825", "Y");
         UserAddress address2 = new UserAddress(2, "深圳市宝安区西部硅谷大厦B座3层（深圳分校）", "1", "王老师", "010-56253825", "N");
-		/*try {
+		try {
 			Thread.sleep(4000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
-		}*/
+		}
         return Arrays.asList(address1,address2);
     }
 }
