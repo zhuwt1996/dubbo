56b40b2d42c00e706a04d5d69a73cdbaae74f00d
zwt <1255284280@qq.com>
2019-06-13 21:08:03 +0800

    多版本实现灰度发布

--- dubbo-user-provider/src/main/java/com/zhuwutao/dubbo/service/impl/UserServiceImpl2.java
+++ dubbo-user-provider/src/main/java/com/zhuwutao/dubbo/service/impl/UserServiceImpl2.java
@@ -9,9 +9,9 @@ import java.util.List;
 /**
  * Created by zhuwutao on 2019-06-05
  */
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl2 implements UserService {
     public List<UserAddress> getUserAddressList(String userId) {
-        System.out.println("UserServiceImpl...old...");
+        System.out.println("UserServiceImpl...new...");
         UserAddress address1 = new UserAddress(1, "北京市昌平区宏福科技园综合楼3层", "1", "李老师", "010-56253825", "Y");
         UserAddress address2 = new UserAddress(2, "深圳市宝安区西部硅谷大厦B座3层（深圳分校）", "1", "王老师", "010-56253825", "N");
 		try {
